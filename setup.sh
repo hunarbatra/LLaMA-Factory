0d2040682ef88534cb650c78177d8ce324ac6e5f
Hunar Batra <i@hunarbatra.com>
2025-04-03 18:03:16 +0100

    update setup bash script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 # SFT LLaMA-Factory Setup
-# git clone --depth 1 https://github.com/hiyouga/LLaMA-Factory.git
-# cd LLaMA-Factory
 
 # install dependencies
 pip install -e ".[torch,metrics]"
@@ -8,10 +6,15 @@ pip uninstall torch -y
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 # to fix cuda issue
 
 # download clevr_r1 dataset
+cd data/
 git lfs install
 git clone https://huggingface.co/datasets/hunarbatra/clevr_r1
 cd clevr_r1
 git lfs pull
+mv clevr_r1.json ../
+mv clevr_r1_data/ ../
+cd ../..
+rm -rf data/clevr_r1
 
 # SFT Qwen2.5-VL-3B-Instruct with clevr_r1
 llamafactory-cli train examples/train_lora/qwen25vl_lora_sft.yaml
